595a9c0e77b84c14bab1c7d30fb8fa2b4ed066c7
Adrian <adr.bak0@gmail.com>
2024-07-16 23:32:16 +0200

    Create game card

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,44 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaAndroid,
+} from "react-icons/fa";
+import { VscTerminalLinux } from "react-icons/vsc";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendoswitch } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "./hooks/useGames";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendoswitch,
+    web: BsGlobe,
+    mac: FaApple,
+    linux: VscTerminalLinux,
+    ios: MdPhoneIphone,
+    android: FaAndroid,
+  };
+  return (
+    <>
+      <HStack marginY={2.5}>
+        {platforms.map((platform) => (
+          <Icon as={iconMap[platform.slug]} color={"gray.500"}></Icon>
+          // <Text>{platform.name}</Text>
+        ))}
+      </HStack>
+    </>
+  );
+};
+
+export default PlatformIconList;
